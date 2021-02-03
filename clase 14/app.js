9a5abc0450c1128f9d6b9346a84708b4bdcad744
Maria Jose Moreno Delgado <mariajosemorenodelgado@Marias-iMac.local>
2021-02-03 13:49:38 -0500

    archivo subido

--- clase 14/app.js
+++ clase 14/app.js
@@ -1,48 +1,42 @@
 import {IMAGES as images} from './initialize.js'
 import {ctx, drawObj, run, start, dT} from './initialize.js'
+let balon = {
+    //PROPIEDADES
+    x:0,
+    y:0,
+    vX: 50,// px por segundo
+    vY: 30,
+    x1: 100,
+    y1: 15,
+    x2:150,
+    y2:30,
+    y3:30,
+    // imagen: undefined,
+    //METODOS
+    dibujarse:function(){
+        // ctx.drawImage(this.imagen, this.x-this.r, this.y-this.r, 2*this.r, 2*this.r);
+        ctx.fillRect(this.x.this.y,100,30)
+        ctx.beginPath();
+        ctx.moveTo(this.x+100,this.y)
+        ctx.lineTo(this.x2,this.y2)
+        ctx.lineTo(this.x1,this.y3)
+    },
+    moverse:function(){
+        this.x = this.x + this.vX * dT/1000;
+        this.y = this.y + this.vY * dT/1000;
+        this.x1 = this.y + this.vX * dT/1000;
+        this.x2= this.y + this.vX* dT/1000;
+        this.y1= this.y + this.vY * dT/1000;
+        this.y2= this.y3 + this.vY * dT/1000;
+    }
 
+}
+run()
 // CREACIón del objeto balón
 // PROPIEDADES> x, y, vX, vY, r, imagen
 // METODOS> dibujarse
 
 // console.log(images)
 
-let lapiz ={
-    x:100,
-    y:100,
-    r:15,
-    vX: 50,// px por segundo
-    vY: -50,
-    dibujarse:function(){
-        // ctx.drawImage(this.imagen, this.x-this.r, this.y-this.r, 2*this.r, 2*this.r);
-        ctx.beginPath();
-        ctx.moveTo(70,40);
-        ctx.lineTo(10,10);
-        ctx.lineTo(15,20);
-        ctx.lineTo(15,40);
-        ctx.lineTo(50,40);
-        ctx.lineTo(50,20);
-        ctx.lineTo(15,20);
-        ctx.stroke();
-        ctx.fill();
-    },
-    moverse:function(){
-        this.x = this.x + this.vX * dT/1000;
-        this.y = this.y + this.vY * dT/1000;
-    }
 
-}
-let lapiz2 = Object.create(lapiz)
-lapiz2.x = 0;
-lapiz2.y = 0;
-lapiz2.vX = 10;
-lapiz2.vY = 10;
-drawObj.draw =  function(){
-    ctx.clearRect(0, 0, 400, 400);
-    lapiz.dibujarse();
-    lapiz2.dibujarse();
-    lapiz.moverse();
-    lapiz2.moverse();
-}
-run()
 
